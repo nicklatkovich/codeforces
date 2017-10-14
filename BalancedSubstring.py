5d14eb72af021911856c6e7dec82405b048bf7e9
NickLatkovich <nicklatkovich@gmail.com>
2017-10-14 13:04:22 +0300

    New decision

--- BalancedSubstring.py
+++ BalancedSubstring.py
@@ -1,64 +1,35 @@
 # 873B
 
 
-class Characteristic:
-    def __init__(self, units: int, zeros: int):
-        self.units = units
-        self.zeros = zeros
-
-    def clone(self):
-        return Characteristic(self.units, self.zeros)
-
-    def isBalanced(self) -> bool:
-        return self.units == self.zeros
+def rIndex(l: list, value) -> int:
+    return len(l) - l[::-1].index(value) - 1
 
 
 def main() -> None:
     n = int(input())
     s = str(input())
-    result = n
-    prevCharacteristic = None
-    subValue = 0
-    while True:
-        if result == 0:
-            print(0)
-            return
-        if result == n:
-            characteristic = Characteristic(0, 0)
-            for char in s:
-                if char == '1':
-                    characteristic.units += 1
-                else:
-                    characteristic.zeros += 1
+    cnt0 = []
+    cnt0Count = 0
+    cnt1 = []
+    cnt1Count = 0
+    b = []
+    for c in s:
+        if c == '1':
+            cnt1Count += 1
         else:
-            characteristic = prevCharacteristic
-            for i in range(result, result + subValue):
-                if s[i] == '1':
-                    characteristic.units -= 1
-                else:
-                    characteristic.zeros -= 1
-        prevCharacteristic = characteristic
-        newCharacteristic = characteristic.clone()
-        iPrev = -1
-        iNext = result - 1
-        subValue = result
-        for i in range(n - result + 1):
-            if i > 0:
-                if s[iPrev] == '1':
-                    newCharacteristic.units -= 1
-                else:
-                    newCharacteristic.zeros -= 1
-                if s[iNext] == '1':
-                    newCharacteristic.units += 1
-                else:
-                    newCharacteristic.zeros += 1
-            if newCharacteristic.isBalanced():
-                print(result)
-                return
-            iPrev += 1
-            iNext += 1
-            subValue = min(subValue, abs(newCharacteristic.units - newCharacteristic.zeros))
-        result -= subValue
+            cnt0Count += 1
+        cnt0.append(cnt0Count)
+        cnt1.append(cnt1Count)
+        b.append(cnt1Count - cnt0Count)
+    usedB = []
+    result = 0
+    for i in range(n):
+        if b[i] in usedB:
+            continue
+        j = rIndex(b, b[i])
+        result = max(result, j - i)
+        usedB.append(b[i])
+    print(result)
 
 
 main()
