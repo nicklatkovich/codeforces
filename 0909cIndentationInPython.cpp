87407c8486996eee6e7194f170e60fe5f978d6a1
NickLatkovich <nicklatkovich@gmail.com>
2017-12-28 11:02:45 +0300

    cpp in python-style :D

--- 0909cIndentationInPython.cpp
+++ 0909cIndentationInPython.cpp
@@ -1,40 +1,37 @@
-#include <iostream>
-const int g = 1000000007;
-struct e {
+#include<iostream>
+const int g=1000000007;
+struct e{
 public:
 	int i;
-	e* n;
-	e(int k = 0) {
-		i = k;
-		n = 0;
+	e*n;
+	e(int k=0){
+		i=k;
+		n=0;
 	}
 };
 using namespace std;
-int main( ) {
+int main(){
 	int n;
-	cin >> n;
-	e* s = new e(1);
-	e* l = s;
-	bool p = true;
-	for (int i = 0; i < n; i++) {
-		if (p) {
-			e* j = s->n, *k = s;
-			while (j) {
-				j->i = (j->i + k->i) % g;
-				j = (k = j)->n;
+	cin>>n;
+	e*s=new e(1),*l=s;
+	bool p=true;
+	for(int i=0;i<n;i++){
+		if(p){
+			e*j=s->n,*k=s;
+			while(j){
+				j->i=(j->i+k->i)%g;
+				j=(k=j)->n;
 			}
-		} else {
-			l = (l->n = new e( ));
-		}
+		}else l=(l->n=new e());
 		char c;
-		cin >> c;
-		p = c == 's';
+		cin>>c;
+		p=c=='s';
 	}
-	e* j = s;
-	int result = 0;
-	while (j) {
-		result = (result + j->i) % g;
-		j = j->n;
+	e*j=s;
+	int result=0;
+	while(j){
+		result=(result+j->i)%g;
+		j=j->n;
 	}
-	cout << result;
+	cout<<result;
 }
--- 0909dMulticoloredDots.cpp
+++ 0909dMulticoloredDots.cpp
@@ -1,63 +1,51 @@
-// #include "stdafx.h"
-#include <iostream>
-#include <string>
-const int g = 1000000007;
-struct e {
+#include<iostream>
+#include<string>
+const int g=1000000007;
+struct e{
 public:
 	int i;
-	e* n;
-	e* p;
+	e*n;
+	e*p;
 	bool d;
-	e(int _i = 0, e* _n = 0, e* _p = 0) {
-		i = _i;
-		n = _n;
-		p = _p;
-		d = false;
+	e(int _i=0,e*_n=0,e*_p=0){
+		i=_i;
+		n=_n;
+		p=_p;
+		d=false;
 	}
 };
 using namespace std;
-int main( ) {
-	e* s = 0;
-	e* f = 0;
+int main(){
+	e*s=0,*f=0;
 	string str;
-	cin >> str;
-	// cout << str;
-	int l = str.length( );
-	for (int i = 0; i < l; i++) {
-		if (i == 0) {
-			s = f = new e(str[i] - 'a', 0, 0);
-		} else {
-			f = ((f->n = new e(str[i] - 'a', 0, f))->p = f)->n;
-		}
+	cin>>str;
+	int l=str.length();
+	for(int i=0;i<l;i++){
+		if(i==0)s=f=new e(str[i]-'a',0,0);
+		else f=((f->n=new e(str[i]-'a',0,f))->p=f)->n;
 	}
-	int r = 0;
-	while (true) {
-		bool _break = true;
-		e* i = s;
-		while (i) {
-			if ((i->p && i->p->i != i->i) || (i->n && i->n->i != i->i)) {
-				i->d = true;
-				_break = false;
+	int r=0;
+	while(true){
+		bool _break=true;
+		e*i=s;
+		while(i){
+			if((i->p&&i->p->i!=i->i)||(i->n&&i->n->i!=i->i)){
+				i->d=true;
+				_break=false;
 			}
-			i = i->n;
+			i=i->n;
 		}
-		if (_break) {
-			break;
-		} else {
-			i = s;
-			while (i) {
-				if (i->d) {
-					if (i->p) {
-						i->p->n = i->n;
-					}
-					if (i->n) {
-						i->n->p = i->p;
-					}
-				}
-				i = i->n;
+		if(_break)break;
+		i = s;
+		while(i){
+			if(i->d){
+				if(i->p)i->p->n=i->n;
+				if(i->n)i->n->p=i->p;
 			}
-			r++;
+			i=i->n;
 		}
+		r++;
+		while(s&&s->d)s=s->n;
 	}
-	cout << r;
+	cout<<r;
 }
